9531ed6021c8f0496abbaa8efa9d44a09dfbc349
Jesper Svensson <jessve-5@student.ltu.se>
2020-01-26 19:23:04 +0100

    rename file to follow standards

--- src/main.java
+++ src/main.java
@@ -1,7 +1,7 @@
 import gui.View;
 import model.Model;
 
-public class main {
+public class Main {
 	public static void main(String[] args) {
 		Model state = new Model(10, 10, 2);
 		new View(state);
