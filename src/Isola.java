219bce084661b4cdc2421781900a90d7c81c647f
Jesper Svensson <Komsta.js@gmail.com>
2020-01-28 11:24:00 +0100

    Test correct git account

--- src/Isola.java
+++ src/Isola.java
@@ -3,7 +3,7 @@ import model.Model;
 
 public class Isola {
 	public static void main(String[] args) {
-		Model state = new Model(10, 10, 2);
+		Model state = new Model(8, 8, 4);
 		new View(state);
 	}
 }
