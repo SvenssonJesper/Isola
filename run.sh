f2c193c6fcd759e042ce8a6e9f81328e1bb5b837
Jesper Svensson <jessve-5@student.ltu.se>
2020-01-30 17:52:11 +0100

    remove debugging print

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ echo "Compiling code ..."
 if [ ! -d "/bin" ]; then
 	mkdir bin
 fi
-javac -d bin -cp src src/Isola.java -Xlint:deprecation
+javac -d bin -cp src src/Isola.java
 
 echo "Running program..."
 java -cp bin Isola
